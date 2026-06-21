3f10bd7688b8625c6eab509bcdd4d86c92e1dd40
Martin Marinov <m.marinov1733@gmail.com>
2026-06-21 23:51:27 +0300

    Add project files.

---
+++ ORMFundamentals/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ORMFundamentals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write C# Application that displays FullName + Age + Salary of all Employees from the Employees from the Research & Development department.
+
+
+        }
+    }
+}
