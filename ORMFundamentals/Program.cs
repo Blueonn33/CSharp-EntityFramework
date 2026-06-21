0f5ac8535c321a77a9ed764d10156a176f62a96d
Martin Marinov <m.marinov1733@gmail.com>
2026-06-21 23:55:54 +0300

    cs

--- ORMFundamentals/Program.cs
+++ ORMFundamentals/Program.cs
@@ -1,12 +1,14 @@
-﻿namespace ORMFundamentals
+﻿using Microsoft.Data.SqlClient;
+
+namespace ORMFundamentals
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             // Write C# Application that displays FullName + Age + Salary of all Employees from the Employees from the Research & Development department.
-
-
+            string connectionString = @"";
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
         }
     }
 }
