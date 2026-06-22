ab8249f4b5bbdfbcad2b0b3e7171209006c622e9
Martin Marinov <m.marinov1733@gmail.com>
2026-06-22 20:06:01 +0300

    извеждане

--- ORMFundamentals/Program.cs
+++ ORMFundamentals/Program.cs
@@ -9,12 +9,31 @@ namespace ORMFundamentals
             // Write C# Application that displays FullName + Age + Salary of all Employees from the Employees from the Research & Development department.
             string connectionString =
                 @"Server=PREDATOR\SQLEXPRESS;Database=SoftUni;Trusted_Connection=True;Encrypt=False;";
+            string sqlQuery = @"SELECT CONCAT(FirstName, ' ', LastName)
+                                AS FullName,
+                                JobTitle,
+                                Salary
+                                FROM Employees
+                                WHERE Salary > 30000";
 
             using
             SqlConnection sqlConnection = new SqlConnection(connectionString);
             sqlConnection.Open();
 
-            Console.WriteLine("Connection opened successfully.");
+            SqlCommand sqlCommand = new SqlCommand(sqlQuery, sqlConnection);
+            using SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+            while (sqlDataReader.Read())
+            {
+                string fullName = sqlDataReader.GetString(0);
+                string jobTitle = sqlDataReader.GetString(1);
+                decimal salary = sqlDataReader.GetDecimal(2);
+
+                Console.WriteLine($"{fullName}, {jobTitle} - {salary:f2}");
+            }
+
+            sqlDataReader.Close();
+            sqlConnection.Close();
         }
     }
 }
