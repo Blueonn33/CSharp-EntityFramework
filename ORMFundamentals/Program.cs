74cf0242bf695688f74c83cb2e431b8bdfc84048
Martin Marinov <m.marinov1733@gmail.com>
2026-06-22 19:53:25 +0300

    connection

--- ORMFundamentals/Program.cs
+++ ORMFundamentals/Program.cs
@@ -7,8 +7,14 @@ namespace ORMFundamentals
         static void Main(string[] args)
         {
             // Write C# Application that displays FullName + Age + Salary of all Employees from the Employees from the Research & Development department.
-            string connectionString = @"";
+            string connectionString =
+                @"Server=PREDATOR\SQLEXPRESS;Database=SoftUni;Trusted_Connection=True;Encrypt=False;";
+
+            using
             SqlConnection sqlConnection = new SqlConnection(connectionString);
+            sqlConnection.Open();
+
+            Console.WriteLine("Connection opened successfully.");
         }
     }
 }
