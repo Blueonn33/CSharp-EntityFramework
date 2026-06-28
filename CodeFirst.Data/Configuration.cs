14f7337e0fc390c70860481b3f34dad94e4aa5eb
Martin Marinov <m.marinov1733@gmail.com>
2026-06-28 20:04:04 +0300

    config file

---
+++ CodeFirst.Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace CodeFirst.Data
+{
+    internal static class Configuration
+    {
+        public const string ConnectionString = "Server=.;Database=BlogDb;TrustServerCertificate=true";
+    }
+}
