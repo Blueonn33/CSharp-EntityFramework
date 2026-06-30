c4248b02084df2f59661bf471d9e716e135f22e4
Martin Marinov <m.marinov1733@gmail.com>
2026-06-30 15:02:21 +0300

    get employees

--- E03.Entity Framework Introduction/StartUp.cs
+++ E03.Entity Framework Introduction/StartUp.cs
@@ -1,9 +1,32 @@
-﻿namespace SoftUni;
+﻿using SoftUni.Data;
+
+namespace SoftUni;
 
 public class StartUp
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
+        SoftUniContext dbContext = new();
 
+        string result = GetEmployeesFullInformation(dbContext);
+
+        Console.WriteLine(result);
+    }
+
+    public static string GetEmployeesFullInformation(SoftUniContext dbContext)
+    {
+        var employees = dbContext.Employees
+            .OrderBy(e => e.EmployeeId)
+            .Select(e => new
+            {
+                e.FirstName,
+                e.LastName,
+                e.MiddleName,
+                e.JobTitle,
+                e.Salary
+            })
+            .ToArray();
+
+        return "Employees fetched";
     }
 }
