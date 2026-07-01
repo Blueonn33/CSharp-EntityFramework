c157c347ec20686b9a3b966d6470cf19cb35d1c0
Martin Marinov <m.marinov1733@gmail.com>
2026-07-01 15:29:15 +0300

    color.cs

---
+++ P02_FootballBetting.Data.Models/Color.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace P02_FootballBetting.Data.Models
+{
+    public class Color
+    {
+        [Key]
+        public int ColorId
+        {
+            get; set;
+        }
+
+        [Required]
+        [MaxLength(40)]
+        public string Name
+        {
+            get;
+            set;
+        } = null!;
+    }
+}
