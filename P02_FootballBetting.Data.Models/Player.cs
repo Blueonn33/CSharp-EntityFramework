1ae3060470673cb46f066409c2a5c6bfdd2083a4
Martin Marinov <m.marinov1733@gmail.com>
2026-07-01 15:46:56 +0300

    player

---
+++ P02_FootballBetting.Data.Models/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace P02_FootballBetting.Data.Models
+{
+    public class Player
+    {
+        [Key]
+        public int PlayerId
+        {
+            get; set;
+        }
+
+        [Required]
+        [MaxLength(150)]
+        public string Name { get; set; } = null!;
+
+        public byte SquadNumber { get; set; }
+
+        public bool IsInjured { get; set; } = false;
+
+        public int PositionId { get; set; }
+
+        public int? TeamId { get; set; }
+        public int TownId { get; set; }
+    }
+}
