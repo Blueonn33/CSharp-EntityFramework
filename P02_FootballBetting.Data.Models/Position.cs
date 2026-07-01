ee9f6411876208c2a6ebb6a912ff1d71919628d7
Martin Marinov <m.marinov1733@gmail.com>
2026-07-01 15:49:58 +0300

    position

---
+++ P02_FootballBetting.Data.Models/Position.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace P02_FootballBetting.Data.Models
+{
+    public class Position
+    {
+        [Key]
+        public int PositionId
+        {
+            get; set;
+        }
+
+        [Required]
+        [MaxLength(30)]
+        public string Name
+        {
+            get;
+            set;
+        } = null!;
+    }
+}
