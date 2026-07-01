9916c892f1102da3a83144d295da2746046601f2
Martin Marinov <m.marinov1733@gmail.com>
2026-07-01 10:50:50 +0300

    reply config

---
+++ CodeFirst.Data/Configurations/ReplyConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using CodeFirst.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CodeFirst.Data.Configurations
+{
+    public class ReplyConfiguration : IEntityTypeConfiguration<Reply>
+    {
+        public void Configure(EntityTypeBuilder<Reply> builder)
+        {
+            builder.HasOne(r => r.Post)
+                .WithMany(p => p.Replies)
+                .HasForeignKey(r => r.PostId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
