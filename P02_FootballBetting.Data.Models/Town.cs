64486b3d4918e5880f99e398e7aab0c9ecd152f8
Martin Marinov <m.marinov1733@gmail.com>
2026-07-01 15:33:27 +0300

    town

---
+++ P02_FootballBetting.Data.Models/Town.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace P02_FootballBetting.Data.Models
+{
+    public class Town
+    {
+        [Key]
+        public int TownId
+        {
+            get; set;
+        }
+
+        [Required]
+        [MaxLength(85)]
+        public string Name { get; set; } = null!;
+
+        public int CountryId { get; set; }
+    }
+}
