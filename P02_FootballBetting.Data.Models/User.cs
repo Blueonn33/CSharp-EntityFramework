40af3a790935046665da86bdbaddb36649c0be7a
Martin Marinov <m.marinov1733@gmail.com>
2026-07-01 16:41:17 +0300

    user

---
+++ P02_FootballBetting.Data.Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace P02_FootballBetting.Data.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserId
+        {
+            get; set;
+        }
+
+        [Required]
+        [MaxLength(50)]
+        public string Username { get; set; } = null!;
+
+        [Required]
+        [MaxLength(200)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+
+        public string Password { get; set; } = null!;
+    }
+}
