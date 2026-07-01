0aad7792a3b481a663187f4ff92769ff023c728b
Martin Marinov <m.marinov1733@gmail.com>
2026-07-01 15:35:44 +0300

    country

---
+++ P02_FootballBetting.Data.Models/Country.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace P02_FootballBetting.Data.Models
+{
+    public class Country
+    {
+        [Key]
+        public int CountryId
+        {
+            get; set;
+        }
+
+        [Required]
+        [MaxLength(60)]
+        public string Name
+        {
+            get;
+            set;
+        } = null!;
+    }
+}
