3389073bb1d0a7d70b2ae3167999c04289515559
Martin Marinov <m.marinov1733@gmail.com>
2026-07-02 10:58:26 +0300

    startup

---
+++ P02_FootballBetting/Startup.cs
@@ -0,0 +1,24 @@
+﻿using P02_FootballBetting.Data;
+
+public class Startup
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            using FootballBettingContext dbContext = new();
+
+            // Db only solution
+            // First drop any existing database to ensure sync with latest changes
+            dbContext.Database.EnsureDeleted();
+
+            // Try to create the database from the latest Code First model
+            dbContext.Database.EnsureCreated();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+            throw;
+        }
+    }
+}
