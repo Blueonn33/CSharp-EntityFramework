1104ce0582b2c67efe0f1533bc6fe5f1fe9c3292
Martin Marinov <m.marinov1733@gmail.com>
2026-07-02 15:23:50 +0300

    course

---
+++ StudentSystem/P01_StudentSystem.Data.Models/Models/Course.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace P01_StudentSystem.Data.Models.Models
+{
+    public class Course
+    {
+        [Key]
+        public int CourseId
+        {
+            get; set;
+        }
+
+        [Required]
+        public string Name
+        {
+            get; set;
+        }
+            = null!;
+
+        public string? Description
+        {
+            get; set;
+        }
+
+        [Column(TypeName = "DATETIME2")]
+        public DateTime StartDate
+        {
+            get; set;
+        }
+
+        [Column(TypeName = "DATETIME2")]
+        public DateTime EndDate
+        {
+            get; set;
+        }
+
+        [Required]
+        [Column(TypeName = "DECIMAL(6, 2)")]
+        public decimal Price
+        {
+            get; set;
+        }
+    }
+}
