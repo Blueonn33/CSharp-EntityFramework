efc559c495ed32082b3dd3885a44f2914d8952d3
Martin Marinov <m.marinov1733@gmail.com>
2026-07-05 22:51:59 +0300

    album

--- 04.LINQ-Exercises-MusicHub-6.0/MusicHub/Data/EntityConfiguration/SongConfiguration.cs
+++ 04.LINQ-Exercises-MusicHub-6.0/MusicHub/Data/EntityConfiguration/SongConfiguration.cs
@@ -1,6 +1,20 @@
-﻿namespace MusicHub.Data.EntityConfiguration
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MusicHub.Data.Models;
+
+namespace MusicHub.Data.EntityConfiguration
 {
-    public class SongConfiguration
+    public class SongConfiguration : IEntityTypeConfiguration<Song>
     {
+        public void Configure(EntityTypeBuilder<Song> entity)
+        {
+            entity.HasKey(x => x.Id);
+
+            entity.Property(s => s.Name)
+                .IsRequired()
+                .HasMaxLength(20);
+
+
+        }
     }
 }
---
+++ 04.LINQ-Exercises-MusicHub-6.0/MusicHub/Data/Models/Album.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicHub.Data.Models
+{
+    public class Album
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public DateOnly ReleaseDate { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int? ProducerId { get; set; }
+
+
+    }
+}
