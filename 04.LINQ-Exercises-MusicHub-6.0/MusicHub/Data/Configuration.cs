e08713fc8676a6ddda3ad7943b5337df63a156f2
Martin Marinov <m.marinov1733@gmail.com>
2026-07-05 21:20:34 +0300

    connection string

---
+++ 04.LINQ-Exercises-MusicHub-6.0/MusicHub/Data/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace MusicHub.Data
+{
+    public static class Configuration
+    {
+        public static string ConnectionString =
+            @"Server=PREDATOR\SQLEXPRESS;Database=MusicHub;Trusted_Connection=True;Encrypt=False";
+    }
+}
