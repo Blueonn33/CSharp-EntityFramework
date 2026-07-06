1c94ee9de5394a942f1a1aaafa152722631b3fef
Martin Marinov <m.marinov1733@gmail.com>
2026-07-06 11:09:22 +0300

    song performer

---
+++ 04.LINQ-Exercises-MusicHub-6.0/MusicHub/Data/EntityConfiguration/SongPerformerConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MusicHub.Data.Models;
+
+namespace MusicHub.Data.EntityConfiguration
+{
+    public class SongPerformerConfiguration : IEntityTypeConfiguration<SongPerformer>
+
+    {
+        public void Configure(EntityTypeBuilder<SongPerformer> builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ 04.LINQ-Exercises-MusicHub-6.0/MusicHub/Data/Models/SongPerformer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicHub.Data.Models
+{
+    public class SongPerformer
+    {
+        public int SongId { get; set; }
+
+        public int PerformerId { get; set; }
+    }
+}
