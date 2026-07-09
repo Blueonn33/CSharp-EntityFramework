33bc5d5d260c936a706099d89e66206c6a7e4e3b
Martin Marinov <m.marinov1733@gmail.com>
2026-07-09 15:10:21 +0300

    config

--- EF_Migrations/AcademicRecordsApp/Data/AcademicRecordsDbContext.cs
+++ EF_Migrations/AcademicRecordsApp/Data/AcademicRecordsDbContext.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using AcademicRecordsApp.Data.Models;
+﻿using AcademicRecordsApp.Data.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace AcademicRecordsApp.Data;
@@ -16,15 +14,32 @@ public partial class AcademicRecordsDbContext : DbContext
     {
     }
 
-    public virtual DbSet<Exam> Exams { get; set; }
+    public virtual DbSet<Exam> Exams
+    {
+        get;
+        set;
+    } = null!;
 
-    public virtual DbSet<Grade> Grades { get; set; }
+    public virtual DbSet<Grade> Grades
+    {
+        get;
+        set;
+    } = null!;
 
-    public virtual DbSet<Student> Students { get; set; }
+    public virtual DbSet<Student> Students
+    {
+        get;
+        set;
+    } = null!;
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=PREDATOR\\SQLEXPRESS;Database=AcademicRecordsDB;Trusted_Connection=True;Encrypt=False;");
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder
+                .UseSqlServer(Configuration.ConnectionString);
+        }
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
---
+++ EF_Migrations/AcademicRecordsApp/Data/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace AcademicRecordsApp.Data
+{
+    internal class Configuration
+    {
+        public static string ConnectionString
+            => @"Server=PREDATOR\SQLEXPRESS;Database=AcademicRecordsDB;Trusted_Connection=True;Encrypt=False;";
+    }
+}
