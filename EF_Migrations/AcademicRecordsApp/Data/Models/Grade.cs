520f064e79ed059717b9b6cca4d43f6e0e8df99c
Martin Marinov <m.marinov1733@gmail.com>
2026-07-09 22:40:35 +0300

    attr

--- EF_Migrations/AcademicRecordsApp/Data/Models/Grade.cs
+++ EF_Migrations/AcademicRecordsApp/Data/Models/Grade.cs
@@ -1,28 +1,35 @@
-﻿namespace AcademicRecordsApp.Data.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AcademicRecordsApp.Data.Models;
 
 public class Grade
 {
+    [Key]
     public int Id
     {
         get; set;
     }
 
+    [Column(TypeName = "DECIMAL(3,2)")]
     public decimal Value
     {
         get; set;
     }
 
+    [ForeignKey(nameof(Exam))]
     public int ExamId
     {
         get; set;
     }
 
+    public virtual Exam Exam { get; set; } = null!;
+
+    [ForeignKey(nameof(Student))]
     public int StudentId
     {
         get; set;
     }
-
-    public virtual Exam Exam { get; set; } = null!;
 
     public virtual Student Student { get; set; } = null!;
 }
--- EF_Migrations/AcademicRecordsApp/Data/Models/Student.cs
+++ EF_Migrations/AcademicRecordsApp/Data/Models/Student.cs
@@ -1,12 +1,17 @@
-﻿namespace AcademicRecordsApp.Data.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AcademicRecordsApp.Data.Models;
 
 public class Student
 {
+    [Key]
     public int Id
     {
         get; set;
     }
 
+    [Required]
+    [MaxLength(100)]
     public string FullName { get; set; } = null!;
 
     public virtual ICollection<Grade> Grades
