4ede63b71b88e9b7dbac48e5a5c06df14bce36e2
Martin Marinov <m.marinov1733@gmail.com>
2026-07-10 15:30:00 +0300

    productshop

---
+++ 07.JSON-Processing-Exercises-ProductShop-6.0/ProductShop/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿namespace ProductShop.Models
+{
+    using System.Collections.Generic;
+
+    public class Category
+    {
+        public int Id
+        {
+            get; set;
+        }
+
+        public string Name { get; set; } = null!;
+
+        public virtual ICollection<CategoryProduct> CategoriesProducts
+        {
+            get; set;
+        } = new List<CategoryProduct>();
+    }
+}
---
+++ JSON-Processing/JsonExtension.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Unicode;
+
+namespace JSON_Processing
+{
+    ///<sumary>
+    /// Методи за сериализация и десериализация на JSON
+    /// </sumary>
+
+    public static class JsonExtension
+    {
+        /// <sumary>
+        /// Опции по подразбиране за сериализация на JSON
+        /// </sumary>
+        private static readonly JsonSerializerOptions _jsonOptions = new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true,
+            Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic),
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            WriteIndented = true
+        };
+
+        public static T? FromJson<T>(this string json) =>
+            JsonSerializer.Deserialize<T>(json, _jsonOptions);
+    }
+}
