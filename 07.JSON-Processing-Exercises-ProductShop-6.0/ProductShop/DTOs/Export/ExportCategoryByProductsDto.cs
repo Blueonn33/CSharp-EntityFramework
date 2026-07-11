bb85fa1309c00d6accf4f561b73f2f183ee7a526
Martin Marinov <m.marinov1733@gmail.com>
2026-07-11 13:11:34 +0300

    dto class

--- 07.JSON-Processing-Exercises-ProductShop-6.0/ProductShop/DTOs/Export/ExportCategoryByProductsDto.cs
+++ 07.JSON-Processing-Exercises-ProductShop-6.0/ProductShop/DTOs/Export/ExportCategoryByProductsDto.cs
@@ -1,5 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
 
 namespace ProductShop.DTOs.Export
 {
@@ -7,6 +7,27 @@ namespace ProductShop.DTOs.Export
     {
         [JsonProperty("category")]
         [Required]
-        public string CategoryName { get; set; } = null!;
+        public string CategoryName
+        {
+            get; set;
+        } = null!;
+
+        [JsonProperty("productsCount")]
+        public int ProductsCount
+        {
+            get; set;
+        }
+
+        [JsonProperty("averagePrice")]
+        public string AveragePrice
+        {
+            get; set;
+        } = null!;
+
+        [JsonProperty("totalRevenue")]
+        public string TotalRevenue
+        {
+            get; set;
+        } = null!;
     }
 }
