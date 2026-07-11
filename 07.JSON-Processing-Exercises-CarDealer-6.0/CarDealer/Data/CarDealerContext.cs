441c9c39e75e39d7bebddb2f227223ef34ff1d04
Martin Marinov <m.marinov1733@gmail.com>
2026-07-11 18:50:06 +0300

    dbContext

--- 07.JSON-Processing-Exercises-CarDealer-6.0/CarDealer/Data/CarDealerContext.cs
+++ 07.JSON-Processing-Exercises-CarDealer-6.0/CarDealer/Data/CarDealerContext.cs
@@ -1,5 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
-using CarDealer.Models;
+﻿using CarDealer.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace CarDealer.Data
 {
@@ -13,13 +13,31 @@ namespace CarDealer.Data
             : base(options)
         {
         }
-      
-        public DbSet<Car> Cars { get; set; }
-        public DbSet<Customer> Customers { get; set; }
-        public DbSet<Part> Parts { get; set; }
-        public DbSet<PartCar> PartsCars { get; set; }
-        public DbSet<Sale> Sales { get; set; }
-        public DbSet<Supplier> Suppliers { get; set; }
+
+        public DbSet<Car> Cars
+        {
+            get; set;
+        }
+        public DbSet<Customer> Customers
+        {
+            get; set;
+        }
+        public DbSet<Part> Parts
+        {
+            get; set;
+        }
+        public DbSet<PartCar> PartsCars
+        {
+            get; set;
+        }
+        public DbSet<Sale> Sales
+        {
+            get; set;
+        }
+        public DbSet<Supplier> Suppliers
+        {
+            get; set;
+        }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
@@ -34,6 +52,37 @@ namespace CarDealer.Data
             modelBuilder.Entity<PartCar>(e =>
             {
                 e.HasKey(k => new { k.CarId, k.PartId });
+            });
+
+            // A Customer can buy many Cars.
+
+            modelBuilder.Entity<Customer>(e =>
+            {
+                e.HasKey(c => c.Id);
+
+                e.HasMany(c => c.Sales)
+                    .WithOne(s => s.Customer)
+                    .HasForeignKey(s => s.CustomerId);
+            });
+
+            // One Supplier can supply many Parts and each Part can be delivered by only one Supplier
+
+            modelBuilder.Entity<Supplier>(e =>
+            {
+                e.HasKey(s => s.Id);
+
+                e.HasMany(s => s.Parts)
+                    .WithOne(p => p.Supplier)
+                    .HasForeignKey(p => p.SupplierId);
+            });
+
+            modelBuilder.Entity<Sale>(e =>
+            {
+                e.HasKey(s => s.Id);
+
+                e.HasOne(s => s.Car)
+                    .WithMany(c => c.Sales)
+                    .HasForeignKey(c => c.CarId);
             });
         }
     }
--- 07.JSON-Processing-Exercises-CarDealer-6.0/CarDealer/StartUp.cs
+++ 07.JSON-Processing-Exercises-CarDealer-6.0/CarDealer/StartUp.cs
@@ -1,9 +1,13 @@
-﻿namespace CarDealer
+﻿using CarDealer.Data;
+
+namespace CarDealer
 {
     public class StartUp
     {
         public static void Main()
         {
+            using CarDealerContext dbContext = new();
+
 
         }
     }
