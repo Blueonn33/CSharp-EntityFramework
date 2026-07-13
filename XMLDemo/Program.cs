61a4e705e8f9c2f5d83874a78dd94e3fb8167470
Martin Marinov <m.marinov1733@gmail.com>
2026-07-13 22:23:33 +0300

    first user's name

--- XMLDemo/Program.cs
+++ XMLDemo/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿using System.Xml.Linq;
+
+XDocument doc = XDocument.Load("users.xml");
+var users = doc.Root.Elements();
+
+string name = users
+    .First()
+    .Element("firstName")!
+    .Value;
+
+Console.WriteLine(name);
