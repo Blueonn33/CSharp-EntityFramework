3ec275a133c84e58529d03c2e205762933cc2017
Martin Marinov <m.marinov1733@gmail.com>
2026-07-14 22:06:06 +0300

    create db

--- 08.XML-Processing-Exercises-CarDealer-6.0/CarDealer/StartUp.cs
+++ 08.XML-Processing-Exercises-CarDealer-6.0/CarDealer/StartUp.cs
@@ -1,10 +1,32 @@
-﻿namespace CarDealer
+﻿using CarDealer.Data;
+
+namespace CarDealer
 {
     public class StartUp
     {
         public static void Main()
         {
+            using CarDealerContext dbContext = new CarDealerContext();
+            dbContext.Database.EnsureCreated();
 
+            // Read file
+            string xmlFileName = "suppliers.xml";
+            string xmlFilePath = GetXmlFilePath(xmlFileName);
+            string xmlFileContext = File.ReadAllText(xmlFilePath);
+
+        }
+
+        public static string ImportSuppliers(CarDealerContext dbContext, string inputXml)
+        {
+            return null;
+        }
+
+        private static string GetXmlFilePath(string fileName)
+        {
+            string currentDirectory = Directory.GetCurrentDirectory();
+            string xmlDirectory = Path.Combine(currentDirectory, "../../../Datasets", fileName);
+
+            return Path.GetFullPath(xmlDirectory);
         }
     }
 }
