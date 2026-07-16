4835c6a804618a3dd9fd40390a9ec2243dabfa3e
Martin Marinov <m.marinov1733@gmail.com>
2026-07-16 11:55:31 +0300

    dto

--- 08.XML-Processing-Exercises-CarDealer-6.0/CarDealer/DTOs/Export/ExportSaleDiscountsDto.cs
+++ 08.XML-Processing-Exercises-CarDealer-6.0/CarDealer/DTOs/Export/ExportSaleDiscountsDto.cs
@@ -1,6 +1,10 @@
-﻿namespace CarDealer.DTOs.Export
+﻿using System.Xml.Serialization;
+
+namespace CarDealer.DTOs.Export
 {
+    [XmlType("sale")]
     public class ExportSaleDiscountsDto
     {
+
     }
 }
