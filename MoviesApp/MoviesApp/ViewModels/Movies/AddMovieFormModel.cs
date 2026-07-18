5ff0221e49689280cd70313a80d3e3ccda51fff9
Martin Marinov <m.marinov1733@gmail.com>
2026-07-18 15:06:37 +0300

    validations

--- MoviesApp/MoviesApp/ViewModels/Movies/AddMovieFormModel.cs
+++ MoviesApp/MoviesApp/ViewModels/Movies/AddMovieFormModel.cs
@@ -1,19 +1,44 @@
-﻿namespace MoviesApp.ViewModels.Movies
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MoviesApp.ViewModels.Movies
 {
     public class AddMovieFormModel
     {
+        [Required]
+        [MaxLength(100)]
         public string Title { get; set; } = null!;
 
+        [Required]
+        [MinLength(3)]
+        [MaxLength(50)]
         public string Genre { get; set; } = null!;
 
+        [Required]
+        [MinLength(5)]
+        [MaxLength(100)]
         public string Director { get; set; } = null!;
 
-        public int Duration { get; set; }
+        [Range(1, 500)]
+        public int Duration
+        {
+            get; set;
+        }
 
-        public DateTime ReleaseDate { get; set; }
+        [Required]
+        public DateTime ReleaseDate
+        {
+            get; set;
+        }
 
+        [Required]
+        [MinLength(10)]
+        [MaxLength(2000)]
         public string Description { get; set; } = null!;
 
-        public string? ImageUrl { get; set; }
+        [MaxLength(2048)]
+        public string? ImageUrl
+        {
+            get; set;
+        }
     }
 }
