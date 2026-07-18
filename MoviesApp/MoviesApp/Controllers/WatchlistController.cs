e10180af30d9f0bb0965d558e6562562e674e0e3
Martin Marinov <m.marinov1733@gmail.com>
2026-07-18 22:49:48 +0300

    watchlist

--- MoviesApp/MoviesApp/Controllers/WatchlistController.cs
+++ MoviesApp/MoviesApp/Controllers/WatchlistController.cs
@@ -1,24 +1,44 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MoviesApp.Data;
 using MoviesApp.Services.Interfaces;
 using MoviesApp.ViewModels.Movies;
+using MoviesApp.ViewModels.Watchlist;
 
 namespace MoviesApp.Controllers
 {
     public class WatchlistController : Controller
     {
+        // TODO: Extract the default image URL to a configuration file or a constant class
+        private const string DefaultImageUrl =
+            "https://img.freepik.com/free-vector/cinema-film-production-realistic-transparent-composition-with-isolated-image-clapper-with-empty-fields-vector-illustration_1284-66163.jpg?semt=ais_incoming&w=740&q=80";
+
         private static readonly List<int> _watchlistMovieIds = new();
         private readonly IMoviesService _moviesService;
+        private readonly MoviesAppDbContext _dbContext;
 
-        public WatchlistController(IMoviesService moviesService)
+        public WatchlistController(IMoviesService moviesService, MoviesAppDbContext dbContext)
         {
             _moviesService = moviesService;
+            _dbContext = dbContext;
         }
+
         [HttpGet]
         public IActionResult Index()
         {
-            var movies = MoviesController.DummyMovies
-                .Where(m => _watchlistMovieIds.Contains(m.Id))
-                .ToList();
+            IEnumerable<WatchlistMovieViewModel> movies = _dbContext.Watchlists
+                .AsNoTracking()
+                .Select(w => new WatchlistMovieViewModel()
+                {
+                    Id = w.MovieId,
+                    Title = w.Movie.Title,
+                    Genre = w.Movie.Genre,
+                    Director = w.Movie.Director,
+                    ReleaseDate = w.Movie.ReleaseDate,
+                    ImageUrl = w.Movie.ImageUrl ?? DefaultImageUrl
+                })
+                .Take(25)
+                .ToArray();
 
             return View(movies);
         }
---
+++ MoviesApp/MoviesApp/ViewModels/Watchlist/WatchlistMovieViewModel.cs
@@ -0,0 +1,23 @@
+﻿namespace MoviesApp.ViewModels.Watchlist
+{
+    public class WatchlistMovieViewModel
+    {
+        public int Id
+        {
+            get; set;
+        }
+
+        public string Title { get; set; } = null!;
+
+        public string Genre { get; set; } = null!;
+
+        public string Director { get; set; } = null!;
+
+        public DateTime ReleaseDate { get; set; } = null!;
+
+        public string? ImageUrl
+        {
+            get; set;
+        }
+    }
+}
