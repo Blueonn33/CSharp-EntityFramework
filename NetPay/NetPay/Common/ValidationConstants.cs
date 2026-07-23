5bcb77bc7cf8d00c35053672fb5152e8ee69b342
Martin Marinov <m.marinov1733@gmail.com>
2026-07-23 21:55:08 +0300

    household

---
+++ NetPay/NetPay/Common/ValidationConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace NetPay.Common
+{
+    public static class ValidationConstants
+    {
+        // Household
+        public const int HouseholdContactPersonMinLength = 5;
+        public const int HouseholdContactPersonMaxLength = 50;
+        public const int HouseholdEmailMinLength = 6;
+        public const int HouseholdEmailMaxLength = 80;
+    }
+}
--- NetPay/NetPay/Data/Models/Household.cs
+++ NetPay/NetPay/Data/Models/Household.cs
@@ -1,7 +1,24 @@
-﻿namespace NetPay.Data.Models
+﻿using System.ComponentModel.DataAnnotations;
+using static NetPay.Common.ValidationConstants;
+
+namespace NetPay.Data.Models
 {
     public class Household
     {
+        [Key]
+        public int Id
+        {
+            get; set;
+        }
 
+        [Required]
+        [MinLength(HouseholdContactPersonMinLength)]
+        [MaxLength(HouseholdContactPersonMaxLength)]
+        public string ContactPerson { get; set; } = null!;
+
+        public string? Email
+        {
+            get; set;
+        }
     }
 }
