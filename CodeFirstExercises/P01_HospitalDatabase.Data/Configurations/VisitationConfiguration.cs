54a5d06c2cfd98d5197b8856f2d9adc95b38c93f
Martin Marinov <m.marinov1733@gmail.com>
2026-07-24 10:37:10 +0300

    visitation

---
+++ CodeFirstExercises/P01_HospitalDatabase.Data/Configurations/VisitationConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using P01_HospitalDatabase.Data.Models;
+
+namespace P01_HospitalDatabase.Data.Configurations
+{
+    public class VisitationConfiguration : IEntityTypeConfiguration<Visitation>
+    {
+        public void Configure(EntityTypeBuilder<Visitation> modelBuilder)
+        {
+            modelBuilder.HasKey(v => v.VisitationId);
+
+            modelBuilder.Property(v => v.Comments)
+                .IsUnicode(true);
+        }
+    }
+}
