5debcd3e977c249c4230f68ad17a4e8a7096afd7
Martin Marinov <m.marinov1733@gmail.com>
2026-07-24 10:42:02 +0300

    diagnose

---
+++ CodeFirstExercises/P01_HospitalDatabase.Data/Configurations/DiagnoseConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using P01_HospitalDatabase.Data.Models;
+
+namespace P01_HospitalDatabase.Data.Configurations
+{
+    public class DiagnoseConfiguration : IEntityTypeConfiguration<Diagnose>
+    {
+        public void Configure(EntityTypeBuilder<Diagnose> modelBuilder)
+        {
+            modelBuilder.HasKey(d => d.DiagnoseId);
+
+            modelBuilder.Property(d => d.Name)
+                .IsUnicode(true);
+
+            modelBuilder.Property(d => d.Comments)
+                .IsUnicode(true);
+        }
+    }
+}
