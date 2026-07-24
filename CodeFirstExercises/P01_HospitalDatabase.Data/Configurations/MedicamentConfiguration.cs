e603d5f0e718725d8cc4da7b037e32e4e540d8df
Martin Marinov <m.marinov1733@gmail.com>
2026-07-24 10:44:13 +0300

    medicament

---
+++ CodeFirstExercises/P01_HospitalDatabase.Data/Configurations/MedicamentConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using P01_HospitalDatabase.Data.Models;
+
+namespace P01_HospitalDatabase.Data.Configurations
+{
+    public class MedicamentConfiguration : IEntityTypeConfiguration<Medicament>
+    {
+        public void Configure(EntityTypeBuilder<Medicament> modelBuilder)
+        {
+            modelBuilder.HasKey(m => m.MedicamentId);
+
+            modelBuilder.Property(m => m.Name)
+                .IsUnicode(true);
+        }
+    }
+}
