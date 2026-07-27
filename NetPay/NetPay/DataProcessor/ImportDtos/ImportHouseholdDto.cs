9eed55b0168337afd9f42237ba372c63b8a1516a
Martin Marinov <m.marinov1733@gmail.com>
2026-07-27 11:53:12 +0300

    household dto

---
+++ NetPay/NetPay/DataProcessor/ImportDtos/ImportHouseholdDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+
+namespace NetPay.DataProcessor.ImportDtos
+{
+    [XmlType("Household")]
+    public class ImportHouseholdDto
+    {
+        [Required]
+        [XmlElement("ContactPerson")]
+        public string ContactPerson { get; set; } = null!;
+
+        [XmlElement("Email")]
+        public string? Email { get; set; }
+    }
+}
