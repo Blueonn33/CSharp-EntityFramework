7fb6d7c63385eb47601036006eaf9451c493dc30
Martin Marinov <m.marinov1733@gmail.com>
2026-07-27 15:19:01 +0300

    dtos

---
+++ NetPay/NetPay/DataProcessor/ExportDtos/ExportHouseholdUnpaidExpensesDto.cs
@@ -0,0 +1,22 @@
+﻿using System.Xml.Serialization;
+
+namespace NetPay.DataProcessor.ExportDtos
+{
+    [XmlType("Household")]
+    public class ExportHouseholdUnpaidExpensesDto
+    {
+        [XmlElement("ContactPerson")]
+        public string ContactPerson { get; set; } = null!;
+
+        [XmlElement("Email")]
+        public string? Email
+        {
+            get; set;
+        }
+
+        [XmlElement("PhoneNumber")]
+        public string PhoneNumber { get; set; } = null!;
+
+
+    }
+}
---
+++ NetPay/NetPay/DataProcessor/ExportDtos/ExportUnpaidExpensesDto.cs
@@ -0,0 +1,16 @@
+﻿using System.Xml.Serialization;
+
+namespace NetPay.DataProcessor.ExportDtos
+{
+    [XmlType("Expense")]
+    public class ExportUnpaidExpensesDto
+    {
+        [XmlElement("ExpenseName")]
+        public string ExpenseName { get; set; } = null!;
+
+        public decimal Amount
+        {
+            get; set;
+        }
+    }
+}
