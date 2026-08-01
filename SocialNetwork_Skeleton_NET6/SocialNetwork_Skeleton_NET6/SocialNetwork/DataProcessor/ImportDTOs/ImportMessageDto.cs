dcdb3252ae75c31de3697d9c367a2b209e8522f0
Blueonn33 <m.marinov1733@gmail.com>
2026-08-01 10:15:47 +0300

    MessageDto

---
+++ SocialNetwork_Skeleton_NET6/SocialNetwork_Skeleton_NET6/SocialNetwork/DataProcessor/ImportDTOs/ImportMessageDto.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+
+namespace SocialNetwork.DataProcessor.ImportDTOs
+{
+    [XmlType("Message")]
+    public class ImportMessageDto
+    {
+        [Required]
+        [StringLength(200, MinimumLength = 1)]
+        [XmlElement("Content")]
+        public string Content { get; set; } = null!;
+
+        [Required]
+        [XmlElement("Status")]
+        public string Status
+        {
+            get; set;
+        } = null!;
+
+        [XmlElement("ConversationId")]
+        public int ConversationId
+        {
+            get; set;
+        }
+
+        [XmlElement("SenderId")]
+        public int SenderId
+        {
+            get; set;
+        }
+
+        [Required]
+        [XmlAttribute("SentAt")]
+        public string SentAt
+        {
+            get;
+            set;
+        } = null!;
+    }
+}
