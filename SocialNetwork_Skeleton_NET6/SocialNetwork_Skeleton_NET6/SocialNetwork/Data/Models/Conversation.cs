2b39c109f4c058845015bd468da70c911e8721a6
Blueonn33 <m.marinov1733@gmail.com>
2026-08-01 09:17:00 +0300

    user conversation

---
+++ SocialNetwork_Skeleton_NET6/SocialNetwork_Skeleton_NET6/SocialNetwork/Data/Models/Conversation.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SocialNetwork.Data.Models
+{
+    public class Conversation
+    {
+        [Key]
+        public int Id
+        {
+            get; set;
+        }
+
+        [Required]
+        [MinLength(2)]
+        [MaxLength(30)]
+        public string Title { get; set; } = null!;
+
+        [Required]
+        public DateTime StartedAt
+        {
+            get; set;
+        }
+    }
+}
---
+++ SocialNetwork_Skeleton_NET6/SocialNetwork_Skeleton_NET6/SocialNetwork/Data/Models/UserConversation.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SocialNetwork.Data.Models
+{
+    public class UserConversation
+    {
+        [ForeignKey(nameof(User))]
+        public int UserId
+        {
+            get; set;
+        }
+
+        public virtual User User { get; set; } = null!;
+
+        [ForeignKey(nameof(Conversation))]
+        public int ConversationId
+        {
+            get; set;
+        }
+
+        public virtual Conversation Conversation { get; set; } = null!;
+    }
+}
