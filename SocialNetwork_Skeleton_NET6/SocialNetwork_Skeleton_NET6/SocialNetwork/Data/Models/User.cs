7bf7b4a7e8038fa63b90f437cbdfc5c99b30db0c
Blueonn33 <m.marinov1733@gmail.com>
2026-08-01 09:12:23 +0300

    exam

---
+++ SocialNetwork_Skeleton_NET6/SocialNetwork_Skeleton_NET6/SocialNetwork/Data/Models/User.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SocialNetwork.Data.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id
+        {
+            get; set;
+        }
+
+        [Required]
+        [MinLength(4)]
+        [MaxLength(20)]
+        public string Username { get; set; } = null!;
+
+        [Required]
+        [MinLength(8)]
+        [MaxLength(60)]
+        public string Email { get; set; } = null!;
+
+        [Required]
+        [MinLength(6)]
+        public string Password { get; set; } = null!;
+
+
+    }
+}
