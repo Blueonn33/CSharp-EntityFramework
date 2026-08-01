5dbb85eff0cdabaa0ed678d25562f7423725b7c1
Blueonn33 <m.marinov1733@gmail.com>
2026-08-01 10:18:40 +0300

    PostDto

---
+++ SocialNetwork_Skeleton_NET6/SocialNetwork_Skeleton_NET6/SocialNetwork/DataProcessor/ImportDTOs/ImportPostDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SocialNetwork.DataProcessor.ImportDTOs
+{
+    public class ImportPostDto
+    {
+        [Required]
+        [StringLength(300, MinimumLength = 5)]
+        public string Content { get; set; } = null!;
+
+        [Required]
+        public string CreatedAt { get; set; } = null!;
+
+        public int CreatorId
+        {
+            get; set;
+        }
+    }
+}
